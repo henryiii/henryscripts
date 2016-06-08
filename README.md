98e3e186e7c9f67871d24eeebf35ff25628e370f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2016-06-08 17:34:31 +0200

    Inital setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # henryscripts
+
+
+To install, add the needed line in .bashrc, and link symb:
+
+    ln -s /afs/cern.ch/user/h/hschrein/henryscripts/.bashrc_henry ~/.
+
+
---
+++ bin/bin/rec_setup.sh
@@ -0,0 +1,3 @@
+source /cvmfs/lhcb.cern.ch/group_login.sh
+source $HOME/.local/bin/setup.sh
+export CMTPROJECTPATH=/path/to/upgradeTracking:/cvmfs/lhcbdev.cern.ch/nightlies/lhcb-upgradeTracking/Today:$CMTPROJECTPATH
